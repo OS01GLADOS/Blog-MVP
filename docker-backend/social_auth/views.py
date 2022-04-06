93dd6bbbd82e6f09ca5e57fb9adad9a7b7e11529
OS01GLADOS <no.name.tyan.d@gmail.com>
2022-04-06 18:26:43 +0300

    BLOG-59 done

--- docker-backend/social_auth/views.py
+++ docker-backend/social_auth/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
+import os
+from django.http import JsonResponse, HttpResponseRedirect
 from httplib2 import Http
 import json
 import urllib
@@ -8,8 +9,8 @@ from social_auth.email_login_or_register import login_or_register
 def google_login(request):
     token_request_uri = "https://accounts.google.com/o/oauth2/auth"
     response_type = "code"
-    client_id = '394636177476-a58g5v78eok9lbuifo6pk0t8bicgs84d.apps.googleusercontent.com'
-    redirect_uri = "http://127.0.0.1:8000/api/social/google/"
+    client_id = os.getenv('SOCIAL_AUTH_GOOGLE_CLIENT_ID')
+    redirect_uri = os.getenv('SOCIAL_AUTH_GOOGLE_REDIRECT_URI')
     scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
     url = "{token_request_uri}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}".format(
         token_request_uri=token_request_uri,
@@ -30,13 +31,13 @@ def google_authenticate(request):
         )
 
     access_token_uri = 'https://accounts.google.com/o/oauth2/token'
-    redirect_uri = "http://127.0.0.1:8000/api/social/google/"
+    redirect_uri = os.getenv('SOCIAL_AUTH_GOOGLE_REDIRECT_URI')
     params = urllib.parse.urlencode(
         {
             'code': request.GET['code'],
             'redirect_uri': redirect_uri,
-            'client_id': '394636177476-a58g5v78eok9lbuifo6pk0t8bicgs84d.apps.googleusercontent.com',
-            'client_secret': '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',
+            'client_id': os.getenv('SOCIAL_AUTH_GOOGLE_CLIENT_ID'),
+            'client_secret': os.getenv('SOCIAL_AUTH_GOOGLE_CLIENT_SECRET'),
             'grant_type': 'authorization_code',
         }
     )
@@ -52,10 +53,8 @@ def google_authenticate(request):
             accessToken=token_data['access_token']
         )
     )
-    # this gets the google profile!!
     google_profile = json.loads(content)
 
-    # log the user in
     res = login_or_register(google_profile)
 
     return JsonResponse(res)
